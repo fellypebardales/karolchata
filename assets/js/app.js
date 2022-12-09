c23694e079fc793ec652a1f2a6f88951513a3f69
fellypebardales <86852698+fellypebardales@users.noreply.github.com>
2022-12-09 18:17:59 -0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+var img = document.querySelector("#img-item");
+
+var item = document.querySelector("#item1");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img1.png');
+})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+var item = document.querySelector("#item2");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img2.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+    
+    var item = document.querySelector("#item3");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img3.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+    var item = document.querySelector("#item4");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img4.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+    var item = document.querySelector("#item5");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img5.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+    var item = document.querySelector("#item6");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img6.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+    var item = document.querySelector("#item7");
+item.addEventListener ("mouseover", function() {
+    img.setAttribute('src', 'assets/images/img7.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
+
+    var item = document.querySelector("#item8");
+item.addEventListener ("mouseover", function() {
+    var mouse = document.getElementById('objeto');
+    img.setAttribute('src', 'assets/images/img8.png');})
+item.addEventListener ("mouseout", function() {
+    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+
